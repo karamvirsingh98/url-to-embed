230f8b789443211a57ee771fb03a54ffd70fb908
karamvirsingh98 <karamvir.singh98@gmail.com>
2021-08-07 15:08:30 -0700

    adding comments, updating HTML

--- src/conversionService.ts
+++ src/conversionService.ts
@@ -2,9 +2,17 @@ interface URL {
   url: string;
 }
 
+/* 
+Create the service for converting regular video urls to embeddable urls, 
+which only accepts POST requests. Since results don't need to be stored, 
+no database adapters or operations are added. 
+*/
+
 export default class ConversionService {
   async create(data: Pick<URL, "url">) {
     function generateEmbed(url: string) {
+      
+      //for youtube urls
       if (url.includes("youtube" || "youtu.be")) {
         const regExp =
           /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
@@ -13,6 +21,8 @@ export default class ConversionService {
           ? `https://www.youtube.com/embed/${match[7]}`
           : "invalid url";
       }
+
+      //for vimeo urls
       else if (url.includes("vimeo")) {
         const regExp = /(?:vimeo)\.com.*(?:videos|video|channels|)\/([\d]+)/i;
         const match = url.match(regExp);
@@ -20,9 +30,11 @@ export default class ConversionService {
           ? `https://player.vimeo.com/video/${match[1]}`
           : "invalid url";
       }
+
+      //for any other url
       else return 'invalid url'
     }
-
+      //response from api endopint, as either the sucesfully converted url, or 'invalid url'. 
     return generateEmbed(data.url);
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,21 @@ import express from '@feathersjs/express'
 import cors from 'cors'
 import ConversionService from "./conversionService"
 
+//initialise feathers application using express. 
 const app = express(feathers())
 
+//configure application to use JSON parsing, cors, and other REST features.
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 app.configure(express.rest())
 
+//setup routes for html and api endpoint
 app.use("/", express.static(`${__dirname}/public`))
 app.use("/about", express.static(`${__dirname}/public/about.html`));
 app.use('/convert', new ConversionService())
 
+//run app
 app.listen(process.env.PORT || 3030).on("listening", () =>
   console.log("Server On")
 );
