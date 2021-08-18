a5395c775c3d2ab136a28da67c501b2144b98b51
karamvirsingh98 <karamvir.singh98@gmail.com>
2021-08-18 13:09:14 -0700

    remove match constraints

--- src/conversionService.ts
+++ src/conversionService.ts
@@ -17,7 +17,7 @@ export default class ConversionService {
         const regExp =
           /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
         const match = url.match(regExp);
-        return match && match[7].length === 11
+        return match
           ? `https://www.youtube.com/embed/${match[7]}`
           : "invalid url";
       }
@@ -26,7 +26,7 @@ export default class ConversionService {
       else if (url.includes("vimeo")) {
         const regExp = /(?:vimeo)\.com.*(?:videos|video|channels|)\/([\d]+)/i;
         const match = url.match(regExp);
-        return match && match[1].length >= 7
+        return match
           ? `https://player.vimeo.com/video/${match[1]}`
           : "invalid url";
       }
